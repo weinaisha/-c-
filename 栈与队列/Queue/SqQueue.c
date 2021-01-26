e730f30a7c7b76ceb73d217a63dba718d231ed21
weinaisha <weinaisha@sjtu.edu.cn>
2021-01-26 11:08:54 +0800

    '循环顺序队列'

--- 栈与队列/Queue/SqQueue.c
+++ 栈与队列/Queue/SqQueue.c
@@ -3,51 +3,92 @@
 
 #define OK 1
 #define ERROR 0
+#define TRUE 1
+#define FALSE 0
 #define MAXSIZE 6
 
 typedef int Status;
 typedef int ElemType;
 typedef struct {
-    ElemType data[MAXSIZE];
+    ElemType *base;
     int front;
     int rear;
 } SqQueue;
 
 // 构造一个空队列Q
 Status InitQueue(SqQueue *Q) {
-    (*Q).rear = 0;
-    (*Q).front = 0;
+    if(Q == NULL) return ERROR;
+    Q->base = malloc(sizeof(ElemType) * MAXSIZE);
+    if(!(Q->base)) exit(0);
+    Q->front = Q->rear = 0;
     return OK;
 }
 
-// Status QueueInsert(SqQueue *Q, ElemType e) {
-//     if((*Q).top - (*Q).base == MAXSIZE) return ERROR;
-//     printf("入e=%d", e);
-//     *((*Q).top) = e;
-//     (*Q).top++;
-//     return OK;
-// }
+Status DestroyQueue(SqQueue *Q) {
+    if(Q == NULL) {
+        return ERROR;
+    }
+    
+    if(Q->base) {
+        free(Q->base);
+    }
+    
+    Q->base = NULL;
+    Q->front = Q->rear = 0;
+    
+    return OK;
+}
+
+Status ClearQueue(SqQueue *Q) {
+    Q->front = Q->rear = 0;
+    return OK;
+}
+
+Status QueueEmpty(SqQueue *Q) {
+    if(Q->front == Q->rear)
+        return TRUE;
+    else
+        return FALSE;
+}
+
+int QueueLength(SqQueue Q) {
+    if(Q.base == NULL) return 0;
+    // return Q.rear - Q.front; 非循环队列,尾指针-头指针
+    return (Q.rear - Q.front + MAXSIZE) % MAXSIZE; //循环队列
+}
+
+ElemType GetHead(SqQueue Q) {
+    if(Q.front == Q.rear || Q.base == NULL) return ERROR;
+    return Q.base[Q.front];
+}
 
 Status EnQueue(SqQueue *Q, ElemType e) {
-    (*Q).data[(*Q).rear] = e;
-    (*Q).rear++;
-    printf("front=%d", (*Q).front);
-    printf("rear=%d", (*Q).rear);
+    if(Q == NULL || Q->base == NULL) {
+        return ERROR;
+    }
+    if(Q->front == (Q->rear + 1) % MAXSIZE) return ERROR;
+    Q->base[Q->rear] = e;
+    Q->rear = (Q->rear + 1) % MAXSIZE;
+    printf("尾指针 %d ", Q->rear);
     return OK;
 }
 
 ElemType DeQueue(SqQueue *Q) {
-    ElemType e = (*Q).data[(*Q).front];
-    (*Q).front++;
-    printf("front=%d", (*Q).front);
+    if(QueueEmpty(Q)) return ERROR; // 队空
+    ElemType e = Q->base[Q->front];
+    Q->front = (Q->front + 1) % MAXSIZE;
+    printf("头指针 %d ", Q->front);
     return e;
 }
 
-ElemType GetHead(SqQueue Q) {
-    printf("front=%d", Q.front);
-    return Q.data[Q.front];
+void QueueTraverse(SqQueue Q) {
+    if(QueueEmpty(&Q)) return;
+    int i;
+    i = Q.front;
+    while(i != Q.rear) {
+        printf("%d ", Q.base[i]);
+        i = (i + 1) % MAXSIZE;
+    }
+    printf(" 遍历结束\n");
 }
 
-// int QueueLength(SqQueue Q) {
-//     return Q.top - Q.base;
-// }
--- 栈与队列/Queue/main.c
+++ 栈与队列/Queue/main.c
@@ -5,11 +5,14 @@ int main() {
     Status status;
     status = InitQueue(&Q);
     printf("初始化队列 status=%d, front=%p, rear=%p\n", status, Q.front, Q.rear);
-    for(int i = 1; i < 3; i++) {
+    for(int i = 1; i < 7; i++) {
         status = EnQueue(&Q, i);
         printf("进队：status=%d, 进队元素=%d\n", status, i);
     }
+    printf("头元素：%d\n", GetHead(Q));
     printf("出队：元素=%d\n", DeQueue(&Q));
-    printf("返回对头元素：%d\n", GetHead(Q));
+    status = EnQueue(&Q, 7);
+    printf("进队：status=%d, 进队元素=%d\n", status, 7);
+    QueueTraverse(Q);
     return 0;
 }
